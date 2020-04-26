0e8ab58f4ac2ecde779cac1f5ed1c13f9aef9810
leuk-in-here <64341242+leuk-in-here@users.noreply.github.com>
2020-04-26 11:12:53 +0800

    Update script.py

--- script.py
+++ script.py
@@ -12,14 +12,15 @@ time.sleep(2)
 f = open("input.txt", "r")
 
 ##This is the main section of the code ->opens f up and enumerates the lines till no more lines exist in the file. 
-##time.sleep is a delay function. this is necessary if the input site is trying to prevent too fast of a keystroke input.
+##time.sleep is a delay function. this is necessary if the program/site is trying to prevent too fast of a keystroke input.
 for cnt, line in enumerate(f):
     for char in line:
         keyboard.press(char)
         keyboard.release(char)
-        time.sleep(0.08)
+        time.sleep(0.1)
         
-##This line below codes for a line break at the end of each line, if you want it to print the entire file without line breaks, comment it out.       
+##This line below codes for a line break at the end of each line if not processed by the Py script
+##By default, this is commented in -> test the text file on notepad. If there are two line breaks, comment it out
     time.sleep(0.12)
     keyboard.press(Key.enter)
     keyboard.release(Key.enter)
